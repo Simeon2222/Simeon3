3f76505d8c928ae51ae9e73bf0c91f29f4036fc2
Simeon2222 <[.simeonormanov61@outlook.com] (mailto:.simeonormanov61@outlook.com)>
2025-12-19 15:33:29 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MusicFileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -15,5 +16,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+Route::get('/music', function () {
+        return Inertia::render('music'); // Render the music
+    })->name('music');
+
+    Route::post('/api/musics', [MusicFileController::class, 'store'])->name('musics.store');
+    Route::get('/api/musics', [MusicFileController::class, 'index'])->name('musics.index');
+    Route::delete('/api/musics/{id}', [MusicFileController::class, 'destroy']);
+    Route::post('/api/musics/{id}', [MusicFileController::class, 'update']);
 
 require __DIR__.'/settings.php';
+
